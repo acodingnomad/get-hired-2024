57d902a2971b55fd2578f778a347559cd45e5ff3
Khristinasheer <khristinasar@gmail.com>
2024-03-08 19:04:06 -0800

    initial homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,25 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    // fontFamily: {
+    //   sans: ["Graphik", "sans-serif"],
+    //   serif: ["Merriweather", "serif"],
+    // },
+    // extend: {
+    //   screens: {
+    //     sm: "480px",
+    //     md: "768px",
+    //     lg: "976px",
+    //     xl: "1440px",
+    //   },
+    //   spacing: {
+    //     128: "32rem",
+    //     144: "36rem",
+    //   },
+    //   borderRadius: {
+    //     "4xl": "2rem",
+    //   },
+    // },
   },
   plugins: [],
 };
